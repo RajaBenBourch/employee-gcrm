5bbbba31c717d297e09d7772fa6e190f3d7eb688
Raja <benbourchraja@gmail.com>
2024-03-12 14:48:33 -0100

    inisialisation mecroservice employee

---
+++ src/main/java/com/example/employeegcrm/EmployeeGcrmApplication.java
@@ -0,0 +1,13 @@
+package com.example.employeegcrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeGcrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeGcrmApplication.class, args);
+    }
+
+}
